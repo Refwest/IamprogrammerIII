0218adfb5e43d3718520b2d263800e53305067ae
Filip Działa <fi.dziala@gmail.com>
2023-07-06 02:38:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 0;
+
+        while (number < 2024)
+        {
+            Console.WriteLine(number);
+            {
+                number++;
+            }
+        }
+
+    }
+}
+
+
