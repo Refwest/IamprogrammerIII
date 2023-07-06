460c45177aaa88583b86cf61471a5b10454785ff
Filip Działa <fi.dziala@gmail.com>
2023-07-07 01:22:29 +0200

    Loop - while, for

--- Program.cs
+++ Program.cs
@@ -11,17 +11,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        int number = 0;
+        Console.WriteLine("Enter a number to sum, or \"-1\" to quit");
+        int number = int.Parse(Console.ReadLine());
+        int sumResult = 0;
 
-        while (number < 2024)
+        while (number != -1)
         {
-            Console.WriteLine(number);
-            {
-                number++;
-            }
+            sumResult += number;
+            Console.WriteLine("Result is {0}. Enter next number to sum, or write \"-1\" to quit", sumResult);
+            number = int.Parse(Console.ReadLine());
+
+        }
+        Console.WriteLine("Result is {0}", sumResult);
+
+        // !For Loop
+
+        int forNumber = 0;
+        for (forNumber = 0; forNumber < 5; forNumber++)
+        {
+            Console.WriteLine(forNumber);
+            Console.WriteLine(" second");
+        }
+        //
+
+            Console.WriteLine("Enter how mony times display hello");
+            int hello = int.Parse(Console.ReadLine());
+
+            int helloooo = 0;
+
+        for (helloooo = 0; hello > helloooo; helloooo++)
+        {
+            Console.WriteLine("hello");
+        }
+
+
         }
 
     }
-}
+
 
 
